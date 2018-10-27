47bbef4c6e704ee8d091f2ce15b348a68b85a184
Elizabeth Parnell <ejparnell@hotmail.com>
2018-10-27 13:13:55 -0400

    stuff that was hard today

--- checklist.js
+++ checklist.js
@@ -12,7 +12,41 @@ let items = [
 ];
 
 
-let appendListItem = function(item){
-    $("#items").append(`<input type="checkbox" name="items" value="${item.id}"> ${item.name}<br>`)
+let appendItems = function(items){
+    $("#items").append(items)
 };
-items.forEach(appendListItem);
+function generatetHtml(items) {
+    let stringToInject = "";
+    items.forEach(element => {
+        let builtHtml = buildHtml(element);
+        stringToInject = stringToInject + builtHtml;
+    });
+    deleteItems();
+    appendItems(stringToInject);
+}
+
+function deleteItems() {
+    $("#items").empty();
+}
+
+function buildHtml(item) {
+    let checkString = "";
+    if (item.checked === true){
+        checkString = "checked";
+    }
+    return `<input type="checkbox" ${checkString} name="items" value="${item.id}"> ${item.name}<br>`;
+}
+function saveItem() {
+  let val = $("#inputBox").val();
+  items.push({
+      id:3, name:val, checked:false
+  })
+  generatetHtml(items);
+  localStorage.setItem("toDoItems", JSON.stringify(items));
+}
+
+items = JSON.parse(localStorage.getItem("toDoItems")) || items;
+
+
+generatetHtml(items);
+generatetHtml(items);
